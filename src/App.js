e37a9611ca0442a5cd3eaf7b70596928c3cdd7d7
GubbyDuo <57397396+GubbyDuo@users.noreply.github.com>
2024-08-28 18:06:21 +1000

    made some changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Axios from "axios";
 import List from "./component/List";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Pause from "./pause.svg";
 import Play from "./play.svg";
 import Skip from "./skip.svg";
 import Queue from "./component/Queue";
@@ -23,18 +22,24 @@ async function loadYoutubePlaylist() {
 }
 
 function App() {
-    const [currentVideo, setCurrentVideo] = useState(null);
     const [playlist, setPlaylist] = useState([]);
     const [queue, setQueue] = useState([]);
-    const [player, setPlayer] = useState(null);
+    // const [player, setPlayer] = useState(null);
+    const playerRef = useRef(null);
 
     function openYtPlayer() {
-        let newPlayer = null;
-        if (!player || player.closed) {
-            newPlayer = window.open("", "_blank");
-            setPlayer(newPlayer);
+        const features = "width=1280,height=720";
+        if (!playerRef.current || playerRef.current.closed) {
+            const newPlayer = window.open("", "_blank", features);
+            if (newPlayer) {
+                playerRef.current = newPlayer;
+            } else {
+                console.error("Failed to open a new player tab.");
+            }
+        } else {
+            playerRef.current.focus();
         }
-        return newPlayer;
+        return playerRef.current;
     }
 
     function playVideoInPlayer() {
@@ -42,19 +47,32 @@ function App() {
 
         if (playerTab) {
             let currentVid = queue[0];
-            setCurrentVideo(currentVid);
-
+            setQueue((prevQueue) => {
+                let newQueue = [...prevQueue];
+                newQueue.shift();
+                return newQueue;
+            });
             let videoId = currentVid.videoId;
             // Open the YouTube video in the player tab
             playerTab.location.href = `https://www.youtube.com/watch?v=${videoId}`;
+        } else {
+            console.error("Failed to open player tab");
+        }
+    }
 
-            // Send a message to the tab that contains the video ID
-            playerTab.onload = function () {
-                playerTab.postMessage(
-                    { type: "PLAY_VIDEO", videoId: videoId },
-                    "*",
-                );
-            };
+    function skipToNextSong() {
+        let playerTab = openYtPlayer();
+
+        if (playerTab) {
+            let currentVid = queue[0];
+            setQueue((prevQueue) => {
+                let newQueue = [...prevQueue];
+                newQueue.shift();
+                return newQueue;
+            });
+            let videoId = currentVid.videoId;
+            // Open the YouTube video in the player tab
+            playerTab.location.href = `https://www.youtube.com/watch?v=${videoId}`;
         } else {
             console.error("Failed to open player tab");
         }
@@ -71,43 +89,6 @@ function App() {
     function clearQueue(song) {
         setQueue([]);
     }
-
-    function onPlayerReady() {}
-
-    function onPlayerStateChange() {}
-
-    useEffect(() => {
-        const loadYoutubeApi = () => {
-            const script = document.createElement("script");
-            script.src = "https://www.youtube.com/iframe_api";
-            script.async = true;
-            document.body.appendChild(script);
-        };
-
-        const createPlayer = () => {
-            if (!player) {
-                new window.YT.Player("player", {
-                    height: "390",
-                    width: "640",
-                    videoId: currentVideo, // Example video ID, can be set dynamically
-                    events: {
-                        onReady: onPlayerReady,
-                        onStateChange: onPlayerStateChange,
-                    },
-                });
-            }
-        };
-
-        if (!window.YT) {
-            loadYoutubeApi();
-        } else {
-            createPlayer();
-        }
-
-        window.onYoutubeIframeAPIReady = () => {
-            createPlayer();
-        };
-    }, []);
 
     useEffect(() => {
         async function fetchYtPlaylist() {
@@ -150,13 +131,19 @@ function App() {
                                 alt="Play-Logo"
                             />
                         </button>
-                        <a>
+                        <button
+                            onClick={() => {
+                                queue.length < 1
+                                    ? noVideoQueued()
+                                    : skipToNextSong();
+                            }}
+                        >
                             <img
                                 className="App-logo"
                                 src={Skip}
                                 alt="Skip-Logo"
                             />
-                        </a>
+                        </button>
                     </div>
                 </header>
             </div>
